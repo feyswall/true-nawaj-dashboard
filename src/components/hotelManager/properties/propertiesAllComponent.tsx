29935d63bc2042f3d79f9509d1f437b0dbc0d3f1
feyswal <vicentmkota91@gmail.com>
2024-12-05 00:18:38 +0300

    room show

--- src/components/hotelManager/properties/propertiesAllComponent.tsx
+++ src/components/hotelManager/properties/propertiesAllComponent.tsx
@@ -6,9 +6,19 @@ import { isAuthorized } from '@/utils/roleUtils'
 import PropertiesTableView from '@/views/hotelManager/properties/PropertiesTableView'
 import { Box, Button } from '@mui/material'
 import Link from 'next/link'
+import { where, WhereFilterOp } from 'firebase/firestore'
 
 const PropertiesAllComponent = () => {
-  const { role } = useAuth()
+  const { role, user } = useAuth()
+
+  type WhereClause = {
+    field: string
+    operator: WhereFilterOp // Type provided by Firebase for comparison operators
+    value: any // Use `any` for flexibility or specify the type based on expected values
+  }
+  const defaultQuery: any = [
+    {field: 'managerId', operator: '==', value: user?.uid }
+  ]
 
   if (!isAuthorized(role, ['hotelManager'])) {
     return <p>Access Denied</p>
@@ -16,14 +26,14 @@ const PropertiesAllComponent = () => {
 
   return (
     <div>
-      <Box>
+      <Box sx={{ marginBottom: 2 }}>
         <Link href={`/hotelManager/properties/register`}>
         <Button variant='contained' color='primary'>
           Register New property
         </Button>
         </Link>
       </Box>
-      <PropertiesTableView />
+      <PropertiesTableView incomeDefaultQueryParams={defaultQuery} />
     </div>
   )
 }
